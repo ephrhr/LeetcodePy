6e4628d11342f8880f3dfa3bda4d3919419429b7
kchen2 <kchen2@pecl.com.tw>
2022-11-04 14:25:29 +0800

    fix: update 104.

---
+++ Solutions/104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(node: Optional[TreeNode], depth=0) -> int:
+            if node is None:
+                return depth
+            return max(dfs(node.left, depth + 1), dfs(node.right, depth + 1))
+        return dfs(root)
+# @lc code=end
