a0861f3ef7156e1112131d98e8d48c95b21313b5
kchen2 <kchen2@pecl.com.tw>
2023-02-24 15:06:05 +0800

    update for better understading

--- Solutions/1675.minimize-deviation-in-array.py
+++ Solutions/1675.minimize-deviation-in-array.py
@@ -9,16 +9,18 @@ import heapq
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
         pq = []
-        for n in nums:
-            heapq.heappush(pq, [n / (n & -n), n])
+        for num in nums:
+            # (num & -num) returns the lowest bit of num (even)
+            # num / (num & -num) returns the samllest odd number that can divide num
+            heapq.heappush(pq, [num / (num & -num), num])
         ans = float('inf')
-        ma = max(n for n, o in pq)
+        maxn = max(num for num, origin in pq)
         while len(pq) == len(nums):
-            n, o = heapq.heappop(pq)
-            ans = min(ans, ma - n)
-            if n % 2 == 1 or n < o:
-                ma = max(ma, n * 2)
-                heapq.heappush(pq, [n * 2, o])
+            num, origin = heapq.heappop(pq)
+            ans = min(ans, maxn - num)
+            if num % 2 == 1 or num < origin:
+                maxn = max(maxn, num * 2)
+                heapq.heappush(pq, [num * 2, origin])
         return int(ans)
 # @lc code=end
 
