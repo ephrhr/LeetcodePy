f438855dd673a7623d84bf2c27fc6b1c55e30c34
kchen2 <kchen2@pecl.com.tw>
2023-04-27 11:27:10 +0800

    update 319. Solution

---
+++ Solutions/319.bulb-switcher.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=319 lang=python3
+#
+# [319] Bulb Switcher
+#
+from math import sqrt
+# @lc code=start
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(sqrt(n))
+# @lc code=end
+
