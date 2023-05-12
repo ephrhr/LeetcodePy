2a11370af720fc9c5a598d38533d12787495a850
kchen2 <kchen2@pecl.com.tw>
2023-05-12 10:56:29 +0800

    update 1035. Solution

--- Solutions/1035.uncrossed-lines.py
+++ Solutions/1035.uncrossed-lines.py
@@ -12,7 +12,7 @@ class Solution:
         m, n = len(nums1), len(nums2)
         memo = [0] * (n + 1)
         for i in range(m):
-            for j in range(n)[::-1]:
+            for j in range(n - 1, -1, -1):
                 if nums1[i] == nums2[j]:
                     memo[j + 1] = memo[j] + 1
             for j in range(n):
