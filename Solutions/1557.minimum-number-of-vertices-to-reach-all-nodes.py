161d71a680a893014e7d89313a4c5886c8a9e918
kchen2 <kchen2@pecl.com.tw>
2023-05-18 09:54:22 +0800

    update 1557. Solution

---
+++ Solutions/1557.minimum-number-of-vertices-to-reach-all-nodes.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1557 lang=python3
+#
+# [1557] Minimum Number of Vertices to Reach All Nodes
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+        reachable = set([t for f, t in edges])
+        return [i for i in range(n) if i not in reachable]
+# @lc code=end
